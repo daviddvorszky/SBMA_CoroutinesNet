cc718c7f7f9457b2578c143a40a6bd804bf7bb15
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-17 15:30:52 +0300

    Download and show image

--- app/src/main/java/com/example/sbma_coroutinesnet/MainActivity.kt
+++ app/src/main/java/com/example/sbma_coroutinesnet/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.example.sbma_coroutinesnet
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sbma_coroutinesnet.ui.theme.SBMA_CoroutinesNetTheme
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val imageURL = URL("https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg")
+        val image: MutableState<Bitmap?> = mutableStateOf(null)
+        lifecycleScope.launch(Dispatchers.Main){
+            image.value = getImage(imageURL)
+        }
+
         setContent {
-            Text("Coroutines net lab")
+            if(image.value == null){
+                Text("Loading image")
+            }else {
+                ShowImage(image.value!!)
+            }
         }
     }
+
+    private suspend fun getImage(url: URL): Bitmap =
+        withContext(Dispatchers.IO) {
+            val img = BitmapFactory.decodeStream(url.openStream())
+            Log.i("pengb", "Image downloaded")
+            return@withContext img
+        }
+}
+
+@Composable
+fun ShowImage(img: Bitmap) {
+        Image(img.asImageBitmap(), "")
 }
